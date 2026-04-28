bfe43fac9a54c364f17316954ae158db25d15ed9
sadakajaria <98814943+SadakaJaria@users.noreply.github.com>
2026-04-28 12:34:24 +0300

    Change CORS policy to allow all origins

--- api/gist.js
+++ api/gist.js
@@ -2,7 +2,7 @@ const GIST_DESCRIPTION = 'Kuveyt Türk Funds - My Portfolio'
 const GIST_FILENAME = 'kt-portfolio.json'
 
 module.exports = async (req, res) => {
-  res.setHeader('Access-Control-Allow-Origin', 'https://sadakajaria.github.io')
+  res.setHeader('Access-Control-Allow-Origin', '*')
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS')
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
 
